589def2a6bb4c70e181cce7ae6f039e65f4e9b90
Piotr Mol <piotradammol@gmail.com>
2018-06-17 12:51:24 +0200

    Configured workspace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+// Angular material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ExpensesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
